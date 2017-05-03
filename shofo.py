ee9c6dd855ead80ce8222331f825486a45a41bd5
jcas1 <jeddcasella@gmail.com>
2017-05-03 16:10:11 +1000

    Add files via upload
    Work in progress - evaluating the shodan API

---
+++ shofo.py
@@ -0,0 +1,22 @@
+import shodan
+import socket
+import subprocess
+import sys
+from datetime import datetime
+
+SHODAN_API_KEY = raw_input("Enter your Shodan API key: ")
+query = raw_input("Enter desired search term e.g. apache: ")
+
+
+api = shodan.Shodan(SHODAN_API_KEY)
+
+try:
+    results = api.search(query)
+ 
+    print 'Results found: %s' % results['total']
+    for result in results['matches']:
+            print 'IP: %s' % result['ip_str']
+            print result['data']
+            print ''
+except shodan.APIError, e:
+        print 'Error: %s' % e
